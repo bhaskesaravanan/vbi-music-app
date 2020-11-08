1ddcfcb94a28ecfdea0d7377537534a23668208f
bhasker saravanan <bhaskesaravanan@gmail.com>
2020-11-08 15:14:09 +0530

    fix

--- main.py
+++ main.py
@@ -28,6 +28,12 @@ app.static_folder = os.path.join(app.root_path, "static")
 
 
 if __name__ == '__main__':
-    from routes import *
+    from views.login_view import LOGIN_VIEW
+    from views.playlist_view import PLAYLIST_VIEW
+    from views.song_view import SONG_VIEW
+
+    app.register_blueprint(LOGIN_VIEW)
+    app.register_blueprint(PLAYLIST_VIEW)
+    app.register_blueprint(SONG_VIEW)
     app.run(debug=True)
 
